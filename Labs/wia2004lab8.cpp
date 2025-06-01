81304add06920fa1a97a8c02a1b307b8c3c45c9e
Wong Yoong Yee <95009744+mackwongyy@users.noreply.github.com>
2025-06-02 02:16:20 +0800

    Updated Lab 8 of WIA2004

--- Labs/wia2004lab8.cpp
+++ Labs/wia2004lab8.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 using namespace std;
 
-void sleep_random(int minimumTime_ms = 100, int maximumTime_ms = 500) {
+void sleep_random(unsigned long long minimumTime_ms, unsigned long long maximumTime_ms) {
     int range = maximumTime_ms - minimumTime_ms + 1;
     int ms = minimumTime_ms + (rand() % range);
     usleep(ms * 1000);
@@ -26,22 +26,22 @@ void* philosopherFunction(void* v_arg) {
     unsigned long long leftIndex  = index, rightIndex = (index + 1ULL) % numberOfPhilosophers;
 
     for(unsigned long long i = 0; i < rounds; i++) {
-        cout << "[Philosopher " << index << "] is thinking (round " << (i + 1) << ")\n";
+        cout << "[Philosopher " << index << "] is thinking (round " << (i + 1) << ")" << endl;
         sleep_random(200, 600);
 
         unsigned long long firstFork  = (leftIndex < rightIndex ? leftIndex : rightIndex), secondFork = (leftIndex < rightIndex ? rightIndex : leftIndex);
         pthread_mutex_lock(&forks[firstFork]);
         pthread_mutex_lock(&forks[secondFork]);
 
-        cout << "[Philosopher " << index << "] has picked up forks (" << leftIndex << ", " << rightIndex << "), eating...\n";
+        cout << "[Philosopher " << index << "] has picked up forks (" << leftIndex << ", " << rightIndex << "), eating..." << endl;
         sleep_random(200, 600);
 
         pthread_mutex_unlock(&forks[firstFork]);
         pthread_mutex_unlock(&forks[secondFork]);
-        cout << "[Philosopher " << index << "] has put down forks (" << leftIndex << ", " << rightIndex << "), thus round " << (i + 1) << " is done.\n";
+        cout << "[Philosopher " << index << "] has put down forks (" << leftIndex << ", " << rightIndex << "), thus round " << (i + 1) << " is done." << endl;
     }
 
-    cout << "[Philosopher " << index << "] Finished all " << rounds << " rounds.\n";
+    cout << "[Philosopher " << index << "] Finished all " << rounds << " rounds." << endl;
     return nullptr;
 }
 
@@ -63,7 +63,7 @@ int main() {
     }
 
     pthread_mutex_t* forks = new pthread_mutex_t[numberOfPhilosophers];
-    for(unsigned long long i = 0; i < numberOfPhilosophers; ++i) {
+    for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
         pthread_mutex_init(&forks[i], nullptr);
     }
 
@@ -82,7 +82,7 @@ int main() {
         pthread_join(threads[i], nullptr);
     }
 
-    cout << "All philosophers have finished dining.\n";
+    cout << "All philosophers have finished dining." << endl;
 
     for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
         pthread_mutex_destroy(&forks[i]);
