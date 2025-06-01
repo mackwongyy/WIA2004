a8c33cdf5f1f4a339ffcfb572dfa030c2fe7567a
Wong Yoong Yee <95009744+mackwongyy@users.noreply.github.com>
2025-06-02 02:09:07 +0800

    Added Labs 7 & 8 of WIA2004

---
+++ Labs/wia2004lab7.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    unsigned long long numberOfFrames = 0, numberOfPages = 0;
+    cout << "Enter the number of frames: ";
+    while(!(cin >> numberOfFrames) || numberOfFrames < 1) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Enter a positive whole number for the number of frames: ";
+    }
+
+    cout << "Enter the number of page references: ";
+    while(!(cin >> numberOfPages) || numberOfPages < 1) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Enter a positive whole number for the number of references: ";
+    }
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Enter all page references in one line, separated by spaces:\n";
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    vector<unsigned long long> pages;
+    unsigned long long x;
+    
+    while(iss >> x) {
+        pages.push_back(x);
+    }
+
+    while(pages.size() != numberOfPages) {
+        cout << "Expected " << numberOfPages << " numbers, but got " << pages.size() << ". Re-enter all page references in one line:\n";
+        pages.clear();
+        getline(cin, line);
+        iss.clear();
+        iss.str(line);
+        
+        while(iss >> x) {
+            pages.push_back(x);
+        }
+    }
+
+    vector<long long> frames(numberOfFrames, -1LL);
+    queue<unsigned long long> fifoQueue;
+    unordered_set<unsigned long long> loadedPages;
+    unordered_map<unsigned long long, unsigned long long> pageToIndex;
+    unsigned long long nextFreeFrame = 0, hits = 0, faults = 0;
+
+    cout << setw(10) << "Reference";
+    for(unsigned long long i = 0; i < numberOfFrames; i++) {
+        cout << setw(10) << ("Frame " + to_string(i + 1));
+    }
+    cout << setw(10) << "  Hit/Fault" << "\n";
+
+    unsigned long long totalColumns = 1 + static_cast<int>(numberOfFrames) + 1;
+    unsigned long long totalWidth = totalColumns * 10;
+    for(unsigned long long i = 0; i < totalWidth + 1; i++) {
+        cout << "-";
+    }
+    cout << endl;
+
+    for(unsigned long long reference : pages) {
+        bool isHit = (loadedPages.count(reference) > 0);
+        
+        if(isHit) {
+            hits++;
+        } else {
+            faults++;
+            
+            if(nextFreeFrame < numberOfFrames) {
+                frames[nextFreeFrame] = reference;
+                fifoQueue.push(reference);
+                loadedPages.insert(reference);
+                pageToIndex[reference] = nextFreeFrame;
+                ++nextFreeFrame;
+            } else {
+                unsigned long long victim = fifoQueue.front();
+                fifoQueue.pop();
+                unsigned long long index = pageToIndex[victim];
+
+                loadedPages.erase(victim);
+                pageToIndex.erase(victim);
+
+                frames[index] = reference;
+                fifoQueue.push(reference);
+                loadedPages.insert(reference);
+                pageToIndex[reference] = index;
+            }
+        }
+
+        cout << setw(10) << reference;
+        for(unsigned long long i = 0; i < numberOfFrames; ++i) {
+            if(frames[i] == -1) {
+                cout << setw(10) << "-";
+            } else {
+                cout << setw(10) << frames[i];
+            }
+        }
+        cout << setw(10) << (isHit ? "   Hit" : "   Fault") << endl;
+    }
+
+    cout << "\nTotal References: " << numberOfPages << "\nHits             :" << hits << "\nFaults            :" << faults << endl;
+    double hitRatio = (100.0 * static_cast<double>(hits)) / static_cast<double>(numberOfPages);
+    cout << fixed << setprecision(2) << "Hit Ratio (%)    :" << hitRatio << endl;
+    return 0;
+}
---
+++ Labs/wia2004lab8.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include <pthread.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void sleep_random(int minimumTime_ms = 100, int maximumTime_ms = 500) {
+    int range = maximumTime_ms - minimumTime_ms + 1;
+    int ms = minimumTime_ms + (rand() % range);
+    usleep(ms * 1000);
+}
+
+struct PhilosopherArguments {
+    unsigned long long index, rounds, numberOfPhilosophers;
+    pthread_mutex_t* forks;
+};
+
+void* philosopherFunction(void* v_arg) {
+    PhilosopherArguments* arguments = static_cast<PhilosopherArguments*>(v_arg);
+    unsigned long long index = arguments->index;
+    unsigned long long rounds = arguments->rounds;
+    unsigned long long numberOfPhilosophers = arguments->numberOfPhilosophers;
+    pthread_mutex_t* forks = arguments->forks;
+    unsigned long long leftIndex  = index, rightIndex = (index + 1ULL) % numberOfPhilosophers;
+
+    for(unsigned long long i = 0; i < rounds; i++) {
+        cout << "[Philosopher " << index << "] is thinking (round " << (i + 1) << ")\n";
+        sleep_random(200, 600);
+
+        unsigned long long firstFork  = (leftIndex < rightIndex ? leftIndex : rightIndex), secondFork = (leftIndex < rightIndex ? rightIndex : leftIndex);
+        pthread_mutex_lock(&forks[firstFork]);
+        pthread_mutex_lock(&forks[secondFork]);
+
+        cout << "[Philosopher " << index << "] has picked up forks (" << leftIndex << ", " << rightIndex << "), eating...\n";
+        sleep_random(200, 600);
+
+        pthread_mutex_unlock(&forks[firstFork]);
+        pthread_mutex_unlock(&forks[secondFork]);
+        cout << "[Philosopher " << index << "] has put down forks (" << leftIndex << ", " << rightIndex << "), thus round " << (i + 1) << " is done.\n";
+    }
+
+    cout << "[Philosopher " << index << "] Finished all " << rounds << " rounds.\n";
+    return nullptr;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    unsigned long long numberOfPhilosophers = 0, rounds = 0;
+    cout << "Enter the number of philosophers: ";
+    while(!(cin >> numberOfPhilosophers) || numberOfPhilosophers < 2ULL) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter a whole number greater than 1: ";
+    }
+
+    cout << "Enter the number of think/eat rounds per philosopher: ";
+    while(!(cin >> rounds) || rounds < 1ULL) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Please enter a positive whole number: ";
+    }
+
+    pthread_mutex_t* forks = new pthread_mutex_t[numberOfPhilosophers];
+    for(unsigned long long i = 0; i < numberOfPhilosophers; ++i) {
+        pthread_mutex_init(&forks[i], nullptr);
+    }
+
+    pthread_t* threads = new pthread_t[numberOfPhilosophers];
+    PhilosopherArguments* arguments = new PhilosopherArguments[numberOfPhilosophers];
+
+    for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
+        arguments[i].index = i;
+        arguments[i].rounds = rounds;
+        arguments[i].numberOfPhilosophers = numberOfPhilosophers;
+        arguments[i].forks = forks;
+        pthread_create(&threads[i], nullptr, philosopherFunction, &arguments[i]);
+    }
+
+    for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
+        pthread_join(threads[i], nullptr);
+    }
+
+    cout << "All philosophers have finished dining.\n";
+
+    for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
+        pthread_mutex_destroy(&forks[i]);
+    }
+    delete[] forks;
+    delete[] threads;
+    delete[] arguments;
+
+    return 0;
+}
