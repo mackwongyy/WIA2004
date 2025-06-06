aa7a7b4cf5ad046b84cfd5b274c7f7173a6a4d33
Wong Yoong Yee <95009744+mackwongyy@users.noreply.github.com>
2025-06-06 22:00:17 +0800

    Updated Lab 8 of WIA2004

--- Labs/wia2004lab8.cpp
+++ Labs/wia2004lab8.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <ctime>
+#include <semaphore.h>
 using namespace std;
 
 void sleep_random(unsigned long long minimumTime_ms, unsigned long long maximumTime_ms) {
@@ -14,22 +15,27 @@ void sleep_random(unsigned long long minimumTime_ms, unsigned long long maximumT
 
 struct PhilosopherArguments {
     unsigned long long index, numberOfRounds, numberOfPhilosophers;
-    pthread_mutex_t* forks;
+    pthread_mutex_t *forks;
 };
 
-void* philosopherFunction(void* v_arg) {
+sem_t waiter;
+
+void* philosopherFunction(void *v_arg) {
     PhilosopherArguments* arguments = static_cast<PhilosopherArguments*>(v_arg);
     unsigned long long index = arguments->index;
     unsigned long long numberOfRounds = arguments->numberOfRounds;
     unsigned long long numberOfPhilosophers = arguments->numberOfPhilosophers;
-    pthread_mutex_t* forks = arguments->forks;
-    unsigned long long leftIndex  = index, rightIndex = (index + 1ULL) % numberOfPhilosophers;
+    pthread_mutex_t *forks = arguments->forks;
+    unsigned long long leftIndex  = index;
+    unsigned long long rightIndex = (index + 1ULL) % numberOfPhilosophers;
 
     for(unsigned long long i = 0; i < numberOfRounds; i++) {
         cout << "[Philosopher " << index << "] is thinking (round " << (i + 1) << ")" << endl;
         sleep_random(200, 600);
+        sem_wait(&waiter);
 
-        unsigned long long firstFork  = (leftIndex < rightIndex ? leftIndex : rightIndex), secondFork = (leftIndex < rightIndex ? rightIndex : leftIndex);
+        unsigned long long firstFork = min(leftIndex, rightIndex), secondFork = max(leftIndex, rightIndex);
+
         pthread_mutex_lock(&forks[firstFork]);
         pthread_mutex_lock(&forks[secondFork]);
 
@@ -38,37 +44,42 @@ void* philosopherFunction(void* v_arg) {
 
         pthread_mutex_unlock(&forks[firstFork]);
         pthread_mutex_unlock(&forks[secondFork]);
+
         cout << "[Philosopher " << index << "] has put down forks (" << leftIndex << ", " << rightIndex << "), thus round " << (i + 1) << " is done." << endl;
+        sem_post(&waiter);
     }
 
-    cout << "[Philosopher " << index << "] Finished all " << numberOfRounds << " numberOfRounds." << endl;
+    cout << "[Philosopher " << index << "] Finished all " << numberOfRounds << " rounds." << endl;
     return nullptr;
 }
 
 int main() {
     srand(static_cast<unsigned>(time(nullptr)));
     unsigned long long numberOfPhilosophers = 0, numberOfRounds = 0;
+
     cout << "Enter the number of philosophers: ";
     while(!(cin >> numberOfPhilosophers) || numberOfPhilosophers < 2ULL) {
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Please enter a whole number greater than 1: ";
+        cout << "Enter a whole number greater than 1: ";
     }
 
     cout << "Enter the number of think/eat rounds per philosopher: ";
     while(!(cin >> numberOfRounds) || numberOfRounds < 1ULL) {
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Please enter a positive whole number: ";
+        cout << "Enter a positive whole number: ";
     }
 
-    pthread_mutex_t* forks = new pthread_mutex_t[numberOfPhilosophers];
+    sem_init(&waiter, 0, numberOfPhilosophers - 1);
+
+    pthread_mutex_t *forks = new pthread_mutex_t[numberOfPhilosophers];
     for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
         pthread_mutex_init(&forks[i], nullptr);
     }
 
-    pthread_t* threads = new pthread_t[numberOfPhilosophers];
-    PhilosopherArguments* arguments = new PhilosopherArguments[numberOfPhilosophers];
+    pthread_t *threads = new pthread_t[numberOfPhilosophers];
+    PhilosopherArguments *arguments = new PhilosopherArguments[numberOfPhilosophers];
 
     for(unsigned long long i = 0; i < numberOfPhilosophers; i++) {
         arguments[i].index = i;
@@ -90,6 +101,8 @@ int main() {
     delete[] forks;
     delete[] threads;
     delete[] arguments;
+
+    sem_destroy(&waiter);
 
     return 0;
 }
